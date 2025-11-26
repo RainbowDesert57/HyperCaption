5f9a14f4389ec1bbbde1d9ddd669da95330165d0
Tanish <tanishj1711@gmail.com>
2025-11-26 21:43:10 +0530

    fix(App.tsx): Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { useRef } from 'react';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header/header.tsx'
 import TextPressure from './components/reactbits/TextPressure/TextPressure.tsx';
